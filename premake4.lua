39e1ab52546f9e2a1f78658beb759c444d5f969c
Martín Lucas Golini <spartanj@gmail.com>
2019-06-14 20:12:15 -0300

    Added the project to allow to build SOIL2 as a shared library.

--- premake4.lua
+++ premake4.lua
@@ -56,7 +56,7 @@ function string.starts(String,Start)
 	if ( _ACTION ) then
 		return string.sub(String,1,string.len(Start))==Start
 	end
-	
+
 	return false
 end
 
@@ -71,7 +71,7 @@ end
 function os_findlib( name )
 	if os.is_real("macosx") then
 		local path = "/Library/Frameworks/" .. name .. ".framework"
-		
+
 		if os.isdir( path ) then
 			return path
 		end
@@ -83,12 +83,12 @@ end
 function get_backend_link_name( name )
 	if os.is_real("macosx") then
 		local fname = name .. ".framework"
-		
+
 		if os_findlib( name ) then -- Search for the framework
 			return fname
 		end
 	end
-	
+
 	return name
 end
 
@@ -97,7 +97,7 @@ solution "SOIL2"
 	targetdir("./bin")
 	configurations { "debug", "release" }
 	objdir("obj/" .. os.get() .. "/")
-	
+
 	project "soil2-static-lib"
 		kind "StaticLib"
 
@@ -108,9 +108,61 @@ solution "SOIL2"
 		else
 			language "C"
 		end
-		
+
 		targetdir("lib/" .. os.get() .. "/")
 		files { "src/SOIL2/*.c" }
+
+		configuration "debug"
+			defines { "DEBUG" }
+			flags { "Symbols" }
+			if not is_vs() then
+				buildoptions{ "-Wall" }
+			end
+			targetname "soil2-debug"
+
+		configuration "release"
+			defines { "NDEBUG" }
+			flags { "Optimize" }
+			targetname "soil2"
+
+	project "soil2-shared-lib"
+		kind "SharedLib"
+
+		if is_vs() then
+			language "C++"
+			buildoptions { "/TP" }
+			defines { "_CRT_SECURE_NO_WARNINGS" }
+		else
+			language "C"
+		end
+
+		targetdir("lib/" .. os.get() .. "/")
+		files { "src/SOIL2/*.c" }
+
+		if os.is("windows") and not is_vs() then
+			links { "mingw32" }
+		end
+
+		configuration "mingw32"
+			links { "mingw32" }
+
+		configuration "windows"
+			links {"opengl32","SDL2"}
+
+		configuration "linux"
+			links {"GL","SDL2"}
+
+		configuration "macosx"
+			links { "OpenGL.framework", "CoreFoundation.framework", get_backend_link_name("SDL2") }
+			buildoptions {"-F /Library/Frameworks", "-F ~/Library/Frameworks"}
+			linkoptions {"-F /Library/Frameworks", "-F ~/Library/Frameworks"}
+			includedirs { "/Library/Frameworks/SDL2.framework/Headers" }
+
+		configuration "haiku"
+			links {"GL","SDL2"}
+
+		configuration "freebsd"
+			links {"GL","SDL2"}
 
 		configuration "debug"
 			defines { "DEBUG" }
@@ -130,7 +182,7 @@ solution "SOIL2"
 		language "C++"
 		links { "soil2-static-lib" }
 		files { "src/test/*.cpp", "src/common/*.cpp" }
-		
+
 		if os.is("windows") and not is_vs() then
 			links { "mingw32" }
 		end
@@ -143,19 +195,19 @@ solution "SOIL2"
 
 		configuration "linux"
 			links {"GL","SDL2"}
-		
+
 		configuration "macosx"
 			links { "OpenGL.framework", "CoreFoundation.framework", get_backend_link_name("SDL2") }
 			buildoptions {"-F /Library/Frameworks", "-F ~/Library/Frameworks"}
 			linkoptions {"-F /Library/Frameworks", "-F ~/Library/Frameworks"}
 			includedirs { "/Library/Frameworks/SDL2.framework/Headers" }
-		
+
 		configuration "haiku"
 			links {"GL","SDL2"}
 
 		configuration "freebsd"
 			links {"GL","SDL2"}
-		
+
 		configuration "debug"
 			defines { "DEBUG" }
 			flags { "Symbols" }
@@ -168,7 +220,7 @@ solution "SOIL2"
 			defines { "NDEBUG" }
 			flags { "Optimize" }
 			targetname "soil2-test-release"
-			
+
 	project "soil2-perf-test"
 		kind "ConsoleApp"
 		language "C++"
@@ -178,7 +230,7 @@ solution "SOIL2"
 		if os.is("windows") and not is_vs() then
 			links { "mingw32" }
 		end
-		
+
 		configuration "mingw32"
 			links { "mingw32" }
 
@@ -187,19 +239,19 @@ solution "SOIL2"
 
 		configuration "linux"
 			links {"GL","SDL2"}
-		
+
 		configuration "macosx"
 			links { "OpenGL.framework", "CoreFoundation.framework", get_backend_link_name("SDL2") }
 			buildoptions {"-F /Library/Frameworks", "-F ~/Library/Frameworks"}
 			linkoptions {"-F /Library/Frameworks", "-F ~/Library/Frameworks"}
 			includedirs { "/Library/Frameworks/SDL2.framework/Headers" }
-	   
+
 		configuration "haiku"
 			links {"GL","SDL2"}
 
 		configuration "freebsd"
 			links {"GL","SDL2"}
-		
+
 		configuration "debug"
 			defines { "DEBUG" }
 			flags { "Symbols" }
